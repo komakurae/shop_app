502b75495bd169fe9dfa8ef17525716b4dd4939e
komakurae <k.selivanov@softonix.org>
2023-12-08 02:58:26 +0200

    changed Image.network to CachedNetworkImage, added widget to show price, extracted some variables (MediaQuery and etc), changed MediaQuery.of to MediaQuery.sizeOf

--- lib/screens/home/products/widgets/product_item.dart
+++ lib/screens/home/products/widgets/product_item.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+import 'package:cached_network_image/cached_network_image.dart';
+
 import 'package:shop_app/models/product/models.dart';
 
 class ProductItem extends StatelessWidget {
@@ -11,34 +14,37 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(5),
-      decoration: ShapeDecoration(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-          side: const BorderSide(
-            color: Colors.grey,
-          ),
-        ),
-        color: Colors.white,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: SizedBox.square(
-              dimension: MediaQuery.of(context).size.height / 4,
-              child: Image.network(
-                product.image,
-              ),
+    final height = MediaQuery.sizeOf(context).height;
+    final theme = Theme.of(context);
+
+    return Column(
+      children: [
+        SizedBox.square(
+          dimension: height / 4,
+          child: DecoratedBox(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: CachedNetworkImage(
+              fit: BoxFit.contain,
+              imageUrl: product.image,
+              placeholder: (context, url) => const CircularProgressIndicator(),
             ),
           ),
-          Text(
-            product.title,
-            style: Theme.of(context).textTheme.labelLarge,
+        ),
+        Text(
+          product.title,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+        ),
+        const Spacer(),
+        Text(
+          '${product.price} US\$',
+          style: theme.textTheme.labelLarge!.copyWith(
+            fontWeight: FontWeight.bold,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
