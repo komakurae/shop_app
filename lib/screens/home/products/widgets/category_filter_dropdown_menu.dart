c4a2395495a77299d02e8b9a403bf4bc8dc6ff03
komakurae <k.selivanov@softonix.org>
2023-12-11 12:29:11 +0200

    added condition in buildWhen

--- lib/screens/home/products/widgets/category_filter_dropdown_menu.dart
+++ lib/screens/home/products/widgets/category_filter_dropdown_menu.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' as foundation;
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,6 +14,13 @@ class CategoryFilterDropDownMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ProductsBloc, ProductsState>(
+      buildWhen: (previous, current) {
+        final isFilterSame = previous.filter == current.filter;
+        final isExtraSame =
+            foundation.listEquals(previous.extraData, current.extraData);
+
+        return !isFilterSame || !isExtraSame;
+      },
       builder: (context, state) {
         return DropdownMenu<Category>(
           width: 300,
