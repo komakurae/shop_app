ac590faf9dd12fca1f433f9bf1701fe1af980c50
komakurae <k.selivanov@softonix.org>
2023-12-11 10:22:59 +0200

    added bottom to sliver app bar for filtering products bu category

--- lib/screens/home/products/products_screen.dart
+++ lib/screens/home/products/products_screen.dart
@@ -6,6 +6,7 @@ import 'package:shop_app/localization/index.dart';
 import 'package:shop_app/models/index.dart';
 import 'package:shop_app/router/index.dart';
 import 'package:shop_app/screens/home/products/products_bloc.dart';
+import 'package:shop_app/screens/home/products/widgets/category_filter_dropdown_menu.dart';
 import 'package:shop_app/screens/home/products/widgets/product_item.dart';
 
 @RoutePage()
@@ -14,7 +15,7 @@ class ProductsScreen extends StatelessWidget implements AutoRouteWrapper {
 
   @override
   Widget wrappedRoute(BuildContext context) {
-    context.read<ProductsBloc>().load();
+    context.read<ProductsBloc>().loadWithExtra();
 
     return this;
   }
@@ -25,63 +26,73 @@ class ProductsScreen extends StatelessWidget implements AutoRouteWrapper {
 
     final productsBloc = context.read<ProductsBloc>();
 
-    return RefreshIndicator(
-      onRefresh: () {
-        return productsBloc.loadAsyncFuture();
-      },
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            floating: true,
-            title: TextField(
-              decoration: const InputDecoration(
-                filled: true,
-                fillColor: Colors.white70,
-                suffixIcon: Icon(Icons.search),
+    return SafeArea(
+      child: RefreshIndicator(
+        onRefresh: () {
+          return productsBloc.loadWithExtraAsyncFuture();
+        },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              bottom: const PreferredSize(
+                preferredSize: Size.fromHeight(50),
+                child: Row(
+                  children: [
+                    CategoryFilterDropDownMenu(),
+                  ],
+                ),
               ),
-              onSubmitted: (value) {
-                productsBloc.search(value);
+              title: TextField(
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white70,
+                  suffixIcon: Icon(Icons.search),
+                ),
+                onSubmitted: (value) {
+                  productsBloc.search(value);
+                },
+              ),
+            ),
+            BlocBuilder<ProductsBloc, ProductsState>(
+              builder: (context, state) {
+                switch (state.status) {
+                  case NetworkStatus.initial:
+                  case NetworkStatus.loading:
+                    return const SliverFillRemaining(
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  case NetworkStatus.success:
+                    final products = state.visibleData;
+
+                    return SliverPadding(
+                      padding: const EdgeInsets.all(10),
+                      sliver: SliverGrid.count(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 15,
+                        crossAxisSpacing: 15,
+                        childAspectRatio: 1 / 1.3,
+                        children: products
+                            .map((product) => ProductItem(product: product))
+                            .toList(),
+                      ),
+                    );
+                  case NetworkStatus.failure:
+                    return SliverFillRemaining(
+                      child: ColoredBox(
+                        color: Colors.red,
+                        child: Center(
+                          child: Text(state.errorMsg),
+                        ),
+                      ),
+                    );
+                }
               },
             ),
-          ),
-          BlocBuilder<ProductsBloc, ProductsState>(
-            builder: (context, state) {
-              switch (state.status) {
-                case NetworkStatus.initial:
-                case NetworkStatus.loading:
-                  return const SliverFillRemaining(
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  );
-                case NetworkStatus.success:
-                  final products = state.visibleData;
-
-                  return SliverPadding(
-                    padding: const EdgeInsets.all(10),
-                    sliver: SliverGrid.count(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 15,
-                      crossAxisSpacing: 15,
-                      childAspectRatio: 1 / 1.3,
-                      children: products
-                          .map((product) => ProductItem(product: product))
-                          .toList(),
-                    ),
-                  );
-                case NetworkStatus.failure:
-                  return SliverFillRemaining(
-                    child: ColoredBox(
-                      color: Colors.red,
-                      child: Center(
-                        child: Text(state.errorMsg),
-                      ),
-                    ),
-                  );
-              }
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/screens/home/products/widgets/category_filter_dropdown_menu.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:shop_app/models/product/models.dart';
+import 'package:shop_app/screens/home/products/products_bloc.dart';
+
+class CategoryFilterDropDownMenu extends StatelessWidget {
+  const CategoryFilterDropDownMenu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProductsBloc, ProductsState>(
+      builder: (context, state) {
+        return DropdownMenu<Category>(
+          hintText: 'Sort by categories',
+          leadingIcon: const Icon(Icons.filter_alt),
+          dropdownMenuEntries: state.extraData
+              .map(
+                (category) => DropdownMenuEntry<Category>(
+                  value: category,
+                  label: category.label,
+                ),
+              )
+              .toList(),
+          onSelected: (category) =>
+              context.read<ProductsBloc>().filter(category ?? Category.none),
+          inputDecorationTheme: Theme.of(context)
+              .inputDecorationTheme
+              .copyWith(border: InputBorder.none),
+        );
+      },
+    );
+  }
+}
