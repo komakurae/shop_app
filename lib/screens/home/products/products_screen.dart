f051303d1178acefff973c8a15014c242cb9f907
komakurae <k.selivanov@softonix.org>
2023-12-12 11:12:18 +0200

    added Scaffold with FloatingActionButton, wrapped ProductItem with GestureDetector

--- lib/screens/home/products/products_screen.dart
+++ lib/screens/home/products/products_screen.dart
@@ -26,72 +26,91 @@ class ProductsScreen extends StatelessWidget implements AutoRouteWrapper {
 
     final productsBloc = context.read<ProductsBloc>();
 
-    return SafeArea(
-      child: RefreshIndicator(
-        onRefresh: () {
-          return productsBloc.loadWithExtraAsyncFuture();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.pushRoute(ProductsModalRoute(item: null));
         },
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              floating: true,
-              bottom: const PreferredSize(
-                preferredSize: Size.fromHeight(50),
-                child: Row(
-                  children: [
-                    CategoryFilterDropDownMenu(),
-                  ],
+        child: const Icon(Icons.add),
+      ),
+
+      // not sure it should be here
+      body: SafeArea(
+        child: RefreshIndicator(
+          onRefresh: () {
+            return productsBloc.loadWithExtraAsyncFuture();
+          },
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                floating: true,
+                bottom: const PreferredSize(
+                  preferredSize: Size.fromHeight(50),
+                  child: Row(
+                    children: [
+                      CategoryFilterDropDownMenu(),
+                    ],
+                  ),
+                ),
+                title: TextField(
+                  decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white70,
+                    suffixIcon: Icon(Icons.search),
+                  ),
+                  onSubmitted: (value) {
+                    productsBloc.search(value);
+                  },
                 ),
               ),
-              title: TextField(
-                decoration: const InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white70,
-                  suffixIcon: Icon(Icons.search),
-                ),
-                onSubmitted: (value) {
-                  productsBloc.search(value);
+              BlocBuilder<ProductsBloc, ProductsState>(
+                builder: (context, state) {
+                  switch (state.status) {
+                    case NetworkStatus.initial:
+                    case NetworkStatus.loading:
+                      return const SliverFillRemaining(
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    case NetworkStatus.success:
+                      final products = state.visibleData;
+
+                      return SliverPadding(
+                        padding: const EdgeInsets.all(10),
+                        sliver: SliverGrid.count(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 15,
+                          crossAxisSpacing: 15,
+                          childAspectRatio: 1 / 1.3,
+                          children: products
+                              .map(
+                                (product) => GestureDetector(
+                                  onLongPress: () {
+                                    context.pushRoute(
+                                      ProductsModalRoute(item: product),
+                                    );
+                                  },
+                                  child: ProductItem(product: product),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      );
+                    case NetworkStatus.failure:
+                      return SliverFillRemaining(
+                        child: ColoredBox(
+                          color: Colors.red,
+                          child: Center(
+                            child: Text(state.errorMsg),
+                          ),
+                        ),
+                      );
+                  }
                 },
               ),
-            ),
-            BlocBuilder<ProductsBloc, ProductsState>(
-              builder: (context, state) {
-                switch (state.status) {
-                  case NetworkStatus.initial:
-                  case NetworkStatus.loading:
-                    return const SliverFillRemaining(
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  case NetworkStatus.success:
-                    final products = state.visibleData;
-
-                    return SliverPadding(
-                      padding: const EdgeInsets.all(10),
-                      sliver: SliverGrid.count(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 15,
-                        crossAxisSpacing: 15,
-                        childAspectRatio: 1 / 1.3,
-                        children: products
-                            .map((product) => ProductItem(product: product))
-                            .toList(),
-                      ),
-                    );
-                  case NetworkStatus.failure:
-                    return SliverFillRemaining(
-                      child: ColoredBox(
-                        color: Colors.red,
-                        child: Center(
-                          child: Text(state.errorMsg),
-                        ),
-                      ),
-                    );
-                }
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
