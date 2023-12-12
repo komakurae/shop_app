a7d7e7ae765a54d58fedc0ac5aa14da2b39ac055
komakurae <k.selivanov@softonix.org>
2023-12-12 11:10:10 +0200

    created SelectInputFormBuilder to use for select menus

---
+++ lib/widgets/form_builder/select_input_form_builder.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stx_flutter_form_bloc/stx_flutter_form_bloc.dart';
+
+class SelectInputFormBuilder<T> extends StatefulWidget {
+  const SelectInputFormBuilder({
+    super.key,
+    this.hintText,
+    this.nextFieldFocusNode,
+    required this.fieldBloc,
+    required this.toLabel,
+  });
+
+  final String? hintText;
+  final FocusNode? nextFieldFocusNode;
+  final SelectFieldBloc<T> fieldBloc;
+  final String Function(T) toLabel;
+
+  @override
+  State<SelectInputFormBuilder<T>> createState() =>
+      _SelectInputFormBuilderState<T>();
+}
+
+class _SelectInputFormBuilderState<T> extends State<SelectInputFormBuilder<T>> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SelectFieldBloc<T>, SelectFieldBlocState<T>>(
+      bloc: widget.fieldBloc,
+      builder: (context, state) {
+        return DropdownMenu<T>(
+          hintText: widget.hintText,
+          initialSelection: widget.fieldBloc.value,
+          onSelected: (value) {
+            widget.fieldBloc.changeValue(value);
+          },
+          dropdownMenuEntries: widget.fieldBloc.options.map((item) {
+            final label = widget.toLabel(item);
+
+            return DropdownMenuEntry<T>(
+              value: item,
+              label: label,
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
