63eab8610de37361ea57b5a5d2808a7dc6d84756
komakurae <k.selivanov@softonix.org>
2023-12-13 10:59:21 +0200

    created cards bloc to manage cards state

---
+++ lib/screens/home/cards/cards_bloc.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart' hide Card;
+import 'package:shop_app/models/cards/models.dart';
+import 'package:shop_app/models/index.dart';
+import 'package:shop_app/repositories/cards_repository.dart';
+
+typedef CardsState = NetworkFilterableState<List<Card>, DateTimeRange>;
+
+class CardsBloc
+    extends NetworkFilterableListBloc<Card, DateTimeRange, CardsState> {
+  CardsBloc({required this.repository})
+      : super(
+          const CardsState(
+            data: [],
+            visibleData: [],
+          ),
+        ) {
+    super.network();
+  }
+
+  final CardsRepository repository;
+
+  @override
+  bool equals(Card item1, Card item2) {
+    return item1.id == item2.id;
+  }
+
+  @override
+  Future<List<Card>> onFilterAsync(DateTimeRange filter) {
+    return repository.getCardsInDateRange(start: filter.start, end: filter.end);
+  }
+
+  @override
+  Future<List<Card>> onLoadAsync() {
+    return repository.getAllCards();
+  }
+}
