5c2f07300dbf412d0211ad2d5aa8510ea2f39115
komakurae <k.selivanov@softonix.org>
2023-12-15 14:55:17 +0200

    created date time form builder

---
+++ lib/widgets/form_builder/date_time_form_builder.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stx_flutter_form_bloc/stx_flutter_form_bloc.dart';
+
+import 'package:shop_app/core/extensions/date_time_extension.dart';
+
+class DateTimeFormBuilder extends StatelessWidget {
+  const DateTimeFormBuilder({
+    super.key,
+    required this.fieldBloc,
+  });
+
+  final DateTimeFieldBloc fieldBloc;
+
+  @override
+  Widget build(BuildContext context) {
+    final currentDateTime = DateTime.now();
+    return BlocBuilder<DateTimeFieldBloc, DateTimeFieldBlocState>(
+      bloc: fieldBloc,
+      builder: (context, state) {
+        return ElevatedButton.icon(
+          icon: const Icon(Icons.calendar_month),
+          label: Text(state.value?.format() ?? 'Select date'),
+          onPressed: () async {
+            final selectedDateTime = await showDatePicker(
+              context: context,
+              initialDate: state.value ?? currentDateTime,
+              firstDate: state.value ?? DateTime(currentDateTime.year - 4),
+              lastDate: currentDateTime,
+            );
+            if (selectedDateTime != null) {
+              fieldBloc.changeValue(selectedDateTime);
+            }
+          },
+        );
+      },
+    );
+  }
+}
